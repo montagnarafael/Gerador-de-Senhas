59cce2cbbe581edc90ae0064b985449908eb1bcb
Rafael <125207826+montagnarafael@users.noreply.github.com>
2023-12-17 21:50:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function generatePassword() {
+    var length = 8;
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var password = "";
+    for (var i = 0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset.charAt(randomIndex);
+    }
+    document.getElementById("password").textContent = password;
+  }
